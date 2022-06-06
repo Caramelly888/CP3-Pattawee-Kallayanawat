4243f0be7c13781a9c11dd54c2dfb6aa64be6833
Caramelly888 <106437496+Caramelly888@users.noreply.github.com>
2022-06-06 22:28:21 +0700

    Add files via upload

---
+++ Exercise4_Pattawee_K.py
@@ -0,0 +1,20 @@
+'''
+F = "Foundation English"
+G = "General Business"
+I = "Introduction to Conmputer Systems"
+C = "Computer Programming"
+'''
+
+F = 80.00
+G = 79.80
+I = 90.75
+C = 89.00
+
+
+
+print("--- YOUR SCORE ---")
+print("Foundation English                 :", F)
+print("General Business                   :", G)
+print("Introduction to Conmputer Systems  :", I)
+print("Computer Programming               :", C)
+
