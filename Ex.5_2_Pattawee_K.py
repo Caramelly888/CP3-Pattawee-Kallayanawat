2cb36dac869c72ab726b2450fd36578a5bdc7060
Caramelly888 <106437496+Caramelly888@users.noreply.github.com>
2022-06-11 14:53:19 +0700

    Add files via upload

---
+++ Ex.5_2_Pattawee_K.py
@@ -0,0 +1,5 @@
+print("Velocity")
+S = int(input("Distance = "))
+T = int(input("Time = "))
+z = S/T
+print("Velocity = ",(z),"km/hr")
---
+++ Ex.5_Pattawee_K.py
@@ -0,0 +1,7 @@
+x = int(input("Fill Number : "))
+y = int(input("Fill Number : "))
+
+print("x + y = ",x+y)
+print("x - y = ",x-y)
+print("x * y = ",x*y)
+print("x / y = ",x/y)
